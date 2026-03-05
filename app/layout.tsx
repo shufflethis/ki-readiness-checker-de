685241e8847eb0d1d0b2107d863a8f559b540e0e
Gorden Wuebbe <tracktronaut@gmail.com>
2026-03-05 13:20:54 -0300

    chore: add Plausible analytics tracking snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,13 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Privacy-friendly analytics by Plausible */}
+        <script async src="https://analytics.polymarkt.de/js/pa-RLCVWJXrblAC9rkvnUAYE.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();`
+          }}
+        />
       </head>
       <body className={`${openSans.variable} font-body antialiased bg-page-bg text-brand-black`}>
         {children}
